8546b6f52e2c7035819b36950a5a055884644aba
Vitor Lima <vitorwlima13@gmail.com>
2023-09-01 01:03:44 -0300

    feat: add user agent header

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ export const StatusInvest = {
       defaultViewport: null,
     })
     const page = await browser.newPage()
+    await page.setExtraHTTPHeaders({
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+    })
     await page.goto(`https://statusinvest.com.br/acoes/${ticker}`, {
       waitUntil: 'domcontentloaded',
     })
