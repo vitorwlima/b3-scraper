777789e6ec040701d5c08acc70e1fdf283799c86
Vitor Lima <vitorwlima13@gmail.com>
2023-09-01 01:14:03 -0300

    feat: update headers

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,20 @@ export const StatusInvest = {
     })
     const page = await browser.newPage()
     await page.setExtraHTTPHeaders({
-      'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+      accept: '*/*',
+      'accept-language':
+        'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es-MX;q=0.6,es;q=0.5',
+      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'x-requested-with': 'XMLHttpRequest',
+      'sec-ch-ua':
+        '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)',
     })
     await page.goto(`https://statusinvest.com.br/acoes/${ticker}`, {
       waitUntil: 'domcontentloaded',
